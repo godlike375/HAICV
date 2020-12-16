382ac84ecb62c2b185da1a54273a228dc878c03c
ProNikUp <filippovnikita4@gmail.com>
2020-12-16 13:17:27 +0300

    new interface

--- people-counting-opencv/people_counter.py
+++ people-counting-opencv/people_counter.py
@@ -20,7 +20,8 @@ import cv2
 
 from tkinter import *
 from PIL import Image, ImageTk
-
+from tkinter.ttk import Combobox 
+import tkinter as tk
 
 
 # load our serialized model from disk
@@ -29,37 +30,61 @@ import Room
 
 
 class App(Frame):
+	
+	def camera(self):
+		newWindow = tk.Toplevel(self.master)
+		newWindow["bg"] = "gray22"
+		labelExample = tk.Label(newWindow, text = "Выберети комнату", fg="green yellow", bg="gray22", font=("Arial", 12)).grid(column=0, row=0)  
+		combobox = Combobox(newWindow, values= [1, 2, 3], state='readonly')
+		combobox.current(1)
+		combobox.grid(column=0, row=2)
+		buttonExample = tk.Button(newWindow, text = "Сохранить", fg="green yellow", bg="gray22", font=("Arial", 12)).grid(column=2, row=2)
+
+	def new_camera(self):
+		newWindow = tk.Toplevel(self.master)
+		newWindow["bg"] = "gray22"
+		ip = tk.Label(newWindow, text = "IP Комнаты:", fg="green yellow", bg="gray22", font=("Arial", 12)).grid(column=0, row=0)
+		ip_v = tk.Entry(newWindow).grid(column=2, row=0)
+		name_room = tk.Label(newWindow, text = "Название комнаты:", fg="green yellow", bg="gray22", font=("Arial", 12)).grid(column=0, row=2)
+		name_room_v = tk.Entry(newWindow).grid(column=2, row=2)
+		buttonExample2 = tk.Button(newWindow, text = "Сохранить", fg="green yellow", bg="gray22", font=("Arial", 12)).grid(column=1, row=4)
+
 	def __init__(self):
 		self.master = Tk()
 		self.master.wm_title("HAICV")
+		self.master["bg"] = "gray22"
 		Frame.__init__(self, self.master)
-		self.label = Label(text="", fg="black", font=("Gotham", 14))
+		self.label = Label(text="", fg="green yellow", bg="gray22", font=("Arial", 14))
 		self.label.grid(row = 0, column=11, padx=0, pady=2)
 		self.update_clock()
-		Label(text="Зашло:", fg="black", font=("Gotham", 14)) \
+		Label(text="Зашло:", fg="green yellow", bg="gray22", font=("Arial", 14)) \
 			.grid(row=0, column=0, padx=0, pady=2)
-		self.up = Label(text="0", fg="black", font=("Gotham", 14))
+		self.up = Label(text="0", fg="green yellow", bg="gray22", font=("Arial", 14))
 		self.up.grid(row=0, column=1, padx=0, pady=2)
-		Label(text="Вышло:", fg="black", font=("Gotham", 14)) \
+		Label(text="Вышло:", fg="green yellow", bg="gray22", font=("Arial", 14)) \
 			.grid(row=0, column=2, padx=0, pady=2)
-		self.down = Label(text="0", fg="black", font=("Gotham", 14))
+		self.down = Label(text="0", fg="green yellow", bg="gray22", font=("Arial", 14))
 		self.down.grid(row=0, column=3, padx=0, pady=2)
-		Label(text="IP:", fg="black", font=("Gotham", 14)) \
-			.grid(row=0, column=4, padx=0, pady=2)
-		Entry(width=30, font=("Gotham", 10)) \
-			.grid(row=0, column=5, columnspan=3)
-		Button(text="Отправить", font=("Gotham", 9)).grid(row=0, column=10)
-		imageFrame = Frame(self.master, width=602, height=600)
+		
+		
+
+		self.cam = Button(text="Выбрать камеру", fg="green yellow", bg="gray22", font=("Arial", 9), command=self.camera).grid(row=0, column=6)
+		self.newcam = Button(text="Добавить камеру", fg="green yellow", bg="gray22", font=("Arial", 9), command=self.new_camera).grid(row=0, column=8)
+		imageFrame = Frame(self.master, width=602, height=600, bg="gray22",)
 		imageFrame.grid(row=2, column=0, columnspan=12, padx=10, pady=2)
 		self.lmain = Label(imageFrame)
 		self.lmain.grid(row=0, column=0)
 		self.cap = cv2.VideoCapture(0)
-
+		
+		
+		
 		self.r = Room.Room(self.master)
 		self.r.loop()
 		self.show_frame()
 		self.master.mainloop()
-
+	
+	
+    	
 
 	def update_clock(self):
 		now = time.strftime("%H:%M")
@@ -77,6 +102,7 @@ class App(Frame):
 
 
 #Set up GUI
+
 
 app=App()
 
